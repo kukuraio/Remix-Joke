16742fdec99954a68f57aa896b1191c47527c260
Mashekwa Sika <msikatema@gmail.com>
2023-04-13 22:11:10 +0200

    Improved styling and added form validation.

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,13 +1,7 @@
-import { db } from "../utils/db.server"
-import {useLoaderData} from '@remix-run/react'
 import { Link } from "@remix-run/react"
+import { db } from "../utils/db.server"
+import {useLoaderData} from '@remix-run/react'            
 
-
-export async function loader() {
-    const data = await db.joke.findMany()
-    
-    return data
-}
 
 type Data = {
     id: string;
@@ -15,43 +9,48 @@ type Data = {
     content: string;
 }
 
+export async function loader() {
+    const data = await db.joke.findMany()
+    
+    return data
+}
 function ShowJoke(a: string) {
-    document.getElementById('d_joke')!.style.display = 'flex';
-    document.getElementById('jk')!.innerHTML = a;
+  document.getElementById('d_joke')!.style.display = 'flex';
+  document.getElementById('jk')!.innerHTML = a;
 }
 
 function HideJoke() {
-    document.getElementById('d_joke')!.style.display = 'none'
+  document.getElementById('d_joke')!.style.display = 'none'
 }
 
-
-export default function Jokes() {
+export default function Joke() {
     const data = useLoaderData()
     
     return (
         <div>
-            <h1 className="text-4xl mb-10">JOKES</h1>
+            <h1 className="text-4xl font-extrabold ml-2 mt-10 mb-10">JOKES</h1>
 
             <div className="flex justify-around">
-            <div className="flex flex-col w-[50%]">
-                <p className="mb-10">Get a random joke.</p>
-                <p className="mb-6">Here are a few more jokes to check out:</p>
-                <ul className="relative">
-                  {
-                    data.map((obj:Data) => {
-                      return <li id={obj.id}><button onClick={() => ShowJoke(obj.content)}>{obj.name}</button></li>
-                    })
-                  }
-                   <div className="hidden flex-col absolute rounded-md top-0 left-0 w-[100%] h-auto pb-4 transition-transform bg-black" id='d_joke'>
-                  <button onClick={HideJoke} className="text-lg text-white">X</button>
-                  <p id='jk' className="text-md"></p>
-                </div>
-
-                </ul>
-               
-                <Link to="/newjoke" className="bg-blue-400 px-2 py-4 w-[40%] text-center round-md">ADD YOU OWN</Link>
-            </div>
            
+            <div className="flex flex-col w-[50%]">
+            <p className="mb-10 text-4xl">Get a random joke.</p>
+            <p className="mb-6 text-lg">Here are a few more jokes to check out:</p>
+            <ul className="relative">
+            {
+                data.map((obj:Data) => {
+                    return <li key={obj.id}><button className="border-2 border-blue-900 hover:border-b-2 hover:border-b-pink-600 transition" onClick={() => ShowJoke(obj.content)}>{obj.name}</button></li>
+                })
+            }
+            <div className="hidden flex-col absolute rounded-md top-0 left-0 w-[100%] h-auto pb-4 transition-transform bg-black" id='d_joke'>
+          <button onClick={HideJoke} className="text-lg text-white">X</button>
+          <p id='jk' className="text-md"></p>
+        </div>
+
+        </ul>
+       
+        <Link to="/newjoke" className="bg-pink-700 px-2 py-4 w-[20%] font-extrabold text-center round-full">ADD YOU OWN</Link>
+    </div>
+            
             <div className="w-[40%]">
             <p className="mb-4">Here's a random joke:</p>
             <p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
--- app/routes/newjoke.tsx
+++ app/routes/newjoke.tsx
@@ -1,21 +1,91 @@
+import { db } from "../utils/db.server"
+import { redirect } from "@remix-run/node" 
+import {useActionData, useLoaderData} from '@remix-run/react'
+
+type Data = {
+    id: string;
+    name: string;
+    content: string;
+}
+
+export async function loader() {
+    const data = await db.joke.findMany()
+    
+    return data
+}
+function ShowJoke(a: string) {
+  document.getElementById('d_joke')!.style.display = 'flex';
+  document.getElementById('jk')!.innerHTML = a;
+}
+
+function HideJoke() {
+  document.getElementById('d_joke')!.style.display = 'none'
+}
+
+
 export default function New() {
+    const data = useLoaderData()
+    const data2 = useActionData()
+    
     return (
-        <div className="flex justify-center">
-            <div className="flex flex-col w-[50%] mt-10">
-                <h1 className="text-6xl leading-8 mb-6">JOKES</h1>
-                <h2 className="text-2xl">Add your own hilarious joke.</h2>
+        <div className="flex justify-around pb-10">
+           
+            <div className="flex flex-col w-[40%] pt-10">
+                <p className="mb-10 text-4xl">Get a random joke.</p>
+                <p className="mb-6 text-lg">Here are a few more jokes to check out:</p>
+                <ul className="relative text-lg leading-8">
+                    {
+                        data.map((obj:Data) => {
+                        return <li key={obj.id}><button className="border-2 border-blue-900 hover:border-b-2 hover:border-b-pink-600 transition" onClick={() => ShowJoke(obj.content)}>{obj.name}</button></li>
+                        })
+                    }
+                    <div className="hidden flex-col absolute rounded-md top-0 left-0 w-[100%] h-auto pb-4 transition-transform bg-black" id='d_joke'>
+                    <button onClick={HideJoke} className="text-lg text-white">X</button>
+                    <p id='jk' className="text-md"></p>
+                    </div>
+
+                 </ul>
+      
+            </div>
+
+
+            <div className="flex flex-col w-[40%] mt-10">
+                <h2 className="text-2xl pb-10">Add your own hilarious joke.</h2>
                 <form method="POST">
-                    <div>
-                    <label htmlFor="name" className="">Name:</label>
-                    <input id="name" type="text" className="border-2 border-gray-800"/>
+                    <div className="pb-10">
+                    <label htmlFor="name" className="">Name:</label> <br />
+                    <input id="name" name="name" type="text" className="text-black border-1 border-gray-800 w-[60%] h-[7vh] rounded-full"/>
+                    {data2?.message && <p>{data2.message}</p>}
                     </div>
-                    <div>
-                    <label htmlFor="content" className="">Content:</label>
-                    <input id="content" type="text-area" className="border-2 border-gray-800" />
+                    <div className="pb-10">
+                    <label htmlFor="content" className="">Content:</label> <br />
+                    <input id="content" name="content" type="text-area" className="text-black border-1 border-gray-800 w-[60%] h-[10vh] rounded-md" />
                     </div>
-                    <button type="submit" className="px-4 py-2 bg-blue-700">Submit</button>
+                    {data2?.message2 && <p>{data2.message2}</p>}
+                    <button type="submit" className="px-4 py-2 bg-blue-900 hover:bg-blue-600 transition">Submit</button>
                 </form>
             </div>
         </div>
     )
 }
+
+
+export async function action({request}) {
+    const formData = await request.formData()
+    const f_data = Object.fromEntries(formData)
+    
+    if(f_data.name.trim().length < 5) {
+        return {message: "Joke title is too short, must be at least 6 characters long."}
+    }
+    if(f_data.content.length < 20) {
+        return {message2: "Joke is too short, must be at least 10 characters long."}
+    }
+
+    await db.joke.create({data: {
+        name: f_data.name,
+        content: f_data.content
+    }})
+
+    return redirect('/jokes')
+
+} 
